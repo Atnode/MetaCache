7a7e6dcb84693c767b808f52a340fc0b24fd9327
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2019-11-14 16:34:52 +0100

    Display a message if JavaScript is disabled

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <html lang="<?php echo $lang; ?>">
 
 <head>
-<meta charset="utf-8" />
+    <meta charset="utf-8" />
 	<title>MetaCache</title>
 	<link rel="shortcut icon" href="img/logo.png">
     <meta name="description" content="<?php echo $description; ?>" />
@@ -29,92 +29,83 @@
     <link href="css/style.css" rel="stylesheet">
 
 </head>
-
 <body>
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand navbar-brand-centered"> <i class="fas fa-save"></i> MetaCache</a>
-            </div>
-
-        </div>
-    </nav>
-
-    <div class="container">
-
-        <div style="float:right;">
-            <p><b class="fas fa-save" style="font-size: 100px;" aria-hidden="true"></b></p>
-        </div>
-
-        <div class="jumbotron">
-
-            <h1 id="ml-title"></h1>
-            <p id="ml-desc"></p>
-        </div>
-
-        <div class="center">
-            <div class="input-group">
-                <span class="input-group-addon" id="basic-addon1">http(s)://</span>
-                <input type="text" class="form-control" name="address" id="address" placeholder="URL..." autofocus="autofocus" autocomplete="off" aria-describedby="basic-addon1">
-            </div><br/>
-            <div class="row">
-                <button type="button" class="btn btn-default" id="googlecache"><i class="fab fa-google"></i> Google Cache</button>
-                <button type="button" class="btn btn-default" id="archiveorg"><i class="fas fa-archive"></i> Archive.org</button>
-                <div class="hidden-lg"><br/></div>
-                <button type="button" class="btn btn-default" id="archivetoday"><i class="fas fa-archive"></i> Archive.today</button>
-                <button type="button" class="btn btn-default" id="webcite"><i class="fas fa-globe"></i> WebCite</button>
-            </div>
-        </div>
-    </div>
-    </div>
-    <br/>
-    <footer class="footer">
-        <div class="container">
-            <span class="text-muted text-white" id="ml-footer"></span>
-        </div>
-    </footer>
-
-    <div id="NoUrl" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title" id="ml-modal-nourl-title"></h4>
-                </div>
-                <div class="modal-body">
-                    <p id="ml-modal-nourl-text"></p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal" id="ml-modal-nourl-btnclose"></button>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    <div id="InvalidUrl" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title" id="ml-modal-invalidurl-title"></h4>
-                </div>
-                <div class="modal-body">
-                    <p id="ml-modal-invalidurl-text"></p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal" id="ml-modal-invalidurl-btnclose"></button>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand navbar-brand-centered"><i class="fas fa-save"></i> MetaCache</a>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="jswarning" style="float:right;">
+			<p><b aria-hidden="true" class="fa fa-exclamation-triangle" style="font-size: 100px;"></b></p>
+		</div>
+		<div class="jumbotron jswarning">
+			<h1>We are sorry, yet MetaCache needs JavaScript support to work.</h1>
+			<p>Nous sommes désolé, mais MetaCache a besoin que le support de JavaScript soit activé pour fonctionner.</p>
+		</div>
+		<div class="main">
+			<div style="float:right;">
+				<p><b aria-hidden="true" class="fas fa-save" style="font-size: 100px;"></b></p>
+			</div>
+			<div class="jumbotron">
+				<h1 id="ml-title"></h1>
+				<p id="ml-desc"></p>
+			</div>
+			<div class="center">
+				<div class="input-group">
+					<span class="input-group-addon" id="basic-addon1">http(s)://</span> <input aria-describedby="basic-addon1" autocomplete="off" autofocus="autofocus" class="form-control" id="url" name="address" placeholder="URL..." type="text">
+				</div><br>
+				<div class="row">
+					<button class="btn btn-default" id="googlecache" type="button"><i class="fab fa-google"></i> Google Cache</button> <button class="btn btn-default" id="archiveorg" type="button"><i class="fas fa-archive"></i> Archive.org</button> <button class="btn btn-default" id="archivetoday" type="button"><i class="fas fa-archive"></i> Archive.today</button> <button class="btn btn-default" id="webcite" type="button"><i class="fas fa-globe"></i> WebCite</button>
+				</div>
+			</div>
+		</div>
+	</div><br>
+	<footer class="footer">
+		<div class="container">
+			<span class="text-muted text-white" id="ml-footer"></span>
+		</div>
+	</footer>
+	<div class="modal fade" id="NoUrl" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button class="close" data-dismiss="modal" type="button">&times;</button>
+					<h4 class="modal-title" id="ml-modal-nourl-title"></h4>
+				</div>
+				<div class="modal-body">
+					<p id="ml-modal-nourl-text"></p>
+				</div>
+				<div class="modal-footer">
+					<button class="btn btn-default" data-dismiss="modal" id="ml-modal-nourl-btnclose" type="button"></button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="InvalidUrl" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button class="close" data-dismiss="modal" type="button">&times;</button>
+					<h4 class="modal-title" id="ml-modal-invalidurl-title"></h4>
+				</div>
+				<div class="modal-body">
+					<p id="ml-modal-invalidurl-text"></p>
+				</div>
+				<div class="modal-footer">
+					<button class="btn btn-default" data-dismiss="modal" id="ml-modal-invalidurl-btnclose" type="button"></button>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 <script src="js/jquery.min.js" type="text/javascript"></script>
 <script src="js/jquery.MultiLanguage.min.js"></script>
 <script>
+    $('div.jswarning').css('display','none');
+    $('div.main').css('visibility','visible');
     $(document).ready(function() {
     		$('a').tooltip('hide');
 					$.MultiLanguage('lang/metacache.json', '<?php echo $lang; ?>')
